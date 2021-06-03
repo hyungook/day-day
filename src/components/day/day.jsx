b2d846341f9754d0557dbec734d03b72e5864bb4
오형욱 <nenonenoberneno@gmail.com>
2021-06-03 20:53:26 +0900

    add Editing

--- src/components/day/day.jsx
+++ src/components/day/day.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { dbService } from '../../firebase'
 
 const Day = ({dayObj, isOwner}) => {
+
+    const [editing, setEditing] = useState(false);
+    const [newDay, setNewDay] = useState(dayObj.text);
+
 
     const onDeleteClick = async() => {
         const ok = window.confirm("Are you want to delete this day?");
@@ -11,21 +15,46 @@ const Day = ({dayObj, isOwner}) => {
             // delete
             await dbService.doc(`dayday/${dayObj.id}`).delete();
         }
-
-
-        
-
     }
+
+    const onEditClick = () => {
+        setEditing((prev) => !prev)
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        console.log(dayObj, newDay);
+        await dbService.doc(`dayday/${dayObj.id}`).update({
+            text:newDay,
+        })
+        setEditing(false);
+    }
+    const onChange = (event) => {
+        const {target:{value},} = event;
+        setNewDay(value);
+    };
 
     return (
         <div>
-            <h4>{dayObj.text}</h4>
-            {isOwner && (
-                <>
-                    <button onClick={onDeleteClick}>Delete</button>
-                    <button>Edit</button>
-                </>
-            )}
+            {
+                editing ? (
+                    <>
+                        <form onSubmit={onSubmit}>
+                            <input type="text" placeholder="Edit " value={newDay} required onChange={onChange} />
+                            <input type="submit" value="Update" />
+                        </form>
+                        <button onClick={onEditClick}>cancel</button>
+                    </>
+                ) : (
+                    <>
+                        <h4>{dayObj.text}</h4>
+                        {isOwner && (
+                            <>
+                                <button onClick={onDeleteClick}>Delete</button>
+                                <button onClick={onEditClick}>Edit</button>
+                            </>
+                        )}
+                </>)
+            }
         </div>
     );
 };
